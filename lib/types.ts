e0de030c9d85e41e53fd9a17d9fa1a82a49ebdaa
salfaidlt <salfaidabire@gmail.com>
2024-12-19 22:02:08 +0000

    formated types.ts file

--- lib/types.ts
+++ lib/types.ts
@@ -1,43 +1,42 @@
 // types.ts
 
 export interface User {
-    id: string;
-    name: string;
-    email: string;
-    reviews: Review[];
-    reviewComments: ReviewComment[];
-    createdAt: Date;
-    reviewLikes: ReviewLike[];
-  }
-  
-  export interface Review {
-    id: string;
-    service: string;
-    comment: string;
-    rating: number;
-    user: User;
-    userId: string;
-    reviewComments: ReviewComment[];
-    createdAt: Date;
-    reviewLikes: ReviewLike[];
-  }
-  
-  export interface ReviewComment {
-    id: string;
-    content: string;
-    user: User;
-    userId: string;
-    review: Review;
-    reviewId: string;
-    createdAt: Date;
-  }
-  
-  export interface ReviewLike {
-    id: string;
-    user: User;
-    userId: string;
-    review: Review;
-    reviewId: string;
-    createdAt: Date;
-  }
-  
+  id: string;
+  name: string;
+  email: string;
+  reviews: Review[];
+  reviewComments: ReviewComment[];
+  createdAt: Date;
+  reviewLikes: ReviewLike[];
+}
+
+export interface Review {
+  id: string;
+  service: string;
+  comment: string;
+  rating: number;
+  user: User;
+  userId: string;
+  reviewComments: ReviewComment[];
+  createdAt: Date;
+  reviewLikes: ReviewLike[];
+}
+
+export interface ReviewComment {
+  id: string;
+  content: string;
+  user: User;
+  userId: string;
+  review: Review;
+  reviewId: string;
+  createdAt: Date;
+}
+
+export interface ReviewLike {
+  id: string;
+  user: User;
+  userId: string;
+  review: Review;
+  reviewId: string;
+  createdAt: Date;
+}
